20f44393ba420e056bab7be9e8b46c9b1f123ef3
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-20 11:15:59 -0300

    added the fetchCoordsByIP function to be called in index.js to retreive long/lat

--- iss.js
+++ iss.js
@@ -7,8 +7,8 @@ const fetchMyIP = function(callback) {
       return;
     }
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP.  Response ${body}`;
-      callback(Error(msg), null);
+
+      callback(Error(`Status Code ${response.statusCode} when fetching IP.  ${body}`), null);
       return;
     }
 
@@ -17,8 +17,28 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, { latitude, longitude });
+  });
+};
 
 
-
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
