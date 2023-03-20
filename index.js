df66dcc5ab643241aff48bb2e85c32a740cb4c8f
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-20 11:15:18 -0300

    added the fetchCoordsByIP function and called it, to retrieve long/lat

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,6 +10,13 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:', ip);
 });
 
+fetchCoordsByIP('24.138.34.98', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:', coordinates);
+});
 
 
 
