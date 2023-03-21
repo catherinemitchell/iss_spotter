9383b8d68951a1dad294e8601d438ffdfd2a0a0b
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-21 08:52:38 -0300

    added fetchISSFlyOverTimes function callback and error code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -18,5 +19,15 @@ fetchCoordsByIP('24.138.34.98', (error, coordinates) => {
   console.log('It worked! Returned coordinates:', coordinates);
 });
 
+const exampleCoords = { latitude: '44.6628901', longitude: '-63.4870121' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked!  Returned flyover times:', passTimes);
+});
 
 
